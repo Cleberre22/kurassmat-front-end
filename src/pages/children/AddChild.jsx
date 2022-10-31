7d9ed022e10dc0d59ccb2cba05e17b31636b114a
Cleberre22 <c.leberre@hotmail.fr>
2022-10-31 10:49:01 +0100

    maj

--- src/pages/children/AddChild.jsx
+++ src/pages/children/AddChild.jsx
@@ -8,6 +8,9 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Avatar from "@mui/material/Avatar";
 import TextField from "@mui/material/TextField";
+import IconButton from "@mui/material/IconButton";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
+import Stack from "@mui/material/Stack";
 
 import Alert from "@mui/material/Box";
 import { GroupSharp } from "@mui/icons-material";
@@ -15,12 +18,16 @@ import Typography from "@mui/material/Typography";
 
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
-import IconButton from "@mui/material/IconButton";
+
 import InputAdornment from "@mui/material/InputAdornment";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import Button from "@mui/material/Button";
+
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 import MenuHeader from "../../components/auth/MenuHeader";
 import Fox from "../../components//Fox";
@@ -29,30 +36,33 @@ import Prince from "../../components/Prince";
 const AddChild = () => {
   const navigate = useNavigate();
 
-  const [firstnameChild, setFirstnameChild] = useState("");
-  const [lastnameChild, setLastnameChild] = useState("");
-//   const [birthDate, setBirthDate] = useState("");
-//   const [imageChild, setImageChild] = useState("");
-//   const [user_id, setUser_id] = useState("");
+  const [firstnameChild, setFirstnameChild] = useState("toto");
+  const [lastnameChild, setLastnameChild] = useState("toto");
+  const [birthDate, setBirthDate] = useState("1999-02-02");
+  const [imageChild, setImageChild] = useState("ggjfj");
+  const [users_id, setUsers_id] = useState(1);
+
+  // const [birthDate, setValue] = React.useState(null);
 
   const [validationError, setValidationError] = useState({});
 
-//   const changeHandler = (event) => {
-//     setImageChild(event.target.files[0]);
-//   };
+  
+
+  // const changeHandler = (event) => {
+  //   setImageChild(event.target.files[0]);
+  // };
 
   //Fonction d'ajout d'une fiche enfant
-  const addChild = async (e) => {
+  const AddChild = async (e) => {
     e.preventDefault();
 
     const formData = new FormData();
 
     formData.append("firstnameChild", firstnameChild);
     formData.append("lastnameChild", lastnameChild);
-    // formData.append("birthDate", birthDate);
-    // formData.append("imageChild", imageChild);
-    // formData.append("user_id", user_id);
-   
+    formData.append("birthDate", birthDate);
+    formData.append("imageChild", imageChild);
+    formData.append("users_id", users_id);
 
     await axios
       .post(`http://localhost:8000/api/childs`, formData)
@@ -64,197 +74,92 @@ const AddChild = () => {
       });
   };
 
-
   return (
     <div>
       <MenuHeader />
       <Box className="main">
         <CssBaseline />
         <Container className="containerProfil">
-          <h1 className="titleProfil">Ajouter un enfant aaa</h1>
+          <h1 className="titleProfil">Ajouter un enfant</h1>
 
           <Box className="boxProfil">
             <Box component="form" onSubmit={AddChild}>
-              <Box className="userCardTopEdit" sx={{ mb: 4 }}>
-              <Avatar
-                        sx={{ width: 100, height: 100 }}
-                        src="avatar.png"
-                      />
+              {/* <Box className="userCardTopEdit" sx={{ mb: 4 }}>
+                <Avatar sx={{ width: 100, height: 100 }} src="avatar.png" />
                 <button type="submit" className="button-87" role="button">
-                 Ajouter une photo
+                  Ajouter une photo
                 </button>
-              </Box>
+              </Box> */}
 
               <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
-                  Prénom:
+                {/* <Grid item xs={12} sm={6}>
                   <TextField
-                    // {...AddChild("firstnameChild", {
-                    //   required: true,
-                    //   maxLength: {
-                    //     value: 20,
-                    //     message: "Prénom: Longueur maximale de 20 caractères",
-                    //   },
-                    // })}
+                    required
+                    fullWidth
                     value={firstnameChild}
-                    onChange={(event) => {
-                      setFirstnameChild(event.target.value);
-                    }}
-                    required
-                    fullWidth
+                            onChange={(event) => {
+                              setFirstnameChild(event.target.value);
+                            }}
                     id="firstnameChild"
+                    label="Prénom"
                     autoFocus
                   />
-                </Grid>
+                </Grid> */}
 
-                <Grid item xs={12} sm={6}>
-                  Nom:
+                {/* <Grid item xs={12} sm={6}>
                   <TextField
-                    // {...AddChild("lastnameChild", {
-                    //   required: true,
-                    //   maxLength: {
-                    //     value: 20,
-                    //     message: "Nom: Longueur maximale de 20 caractères",
-                    //   },
-                    // })}
+                    required
+                    fullWidth
                     value={lastnameChild}
-                    onChange={(event) => {
-                      setLastnameChild(event.target.value);
-                    }}
-                    required
-                    fullWidth
+                            onChange={(event) => {
+                              setLastnameChild(event.target.value);
+                            }}
                     id="lastnameChild"
-                    autoFocus
-                  />
-                </Grid>
-
-                {/* {errors.firstname ? (
-                  <Alert
-                    className="errorsMessage"
-                    sx={{ mt: 2, p: 0, pl: 2 }}
-                    severity="error"
-                  >
-                    {errors.firstname?.message}
-                  </Alert>
-                ) : (
-                  ""
-                )}
-                {errors.lastname ? (
-                  <Alert
-                    className="errorsMessage"
-                    sx={{ mt: 2, p: 0, pl: 2 }}
-                    severity="error"
-                  >
-                    {errors.lastname?.message}
-                  </Alert>
-                ) : (
-                  ""
-                )} */}
-
-                {/* <Grid item xs={12} sm={6}>
-                  Email:
-                  <TextField
-                    {...AddChild("email", {
-                      required: true,
-                      maxLength: {
-                        value: 20,
-                        message: "Prénom: Longueur maximale de 20 caractères",
-                      },
-                    })}
-                    value={email}
-                    onChange={(event) => {
-                      setEmail(event.target.value);
-                    }}
-                    required
-                    fullWidth
-                    id="email"
+                    label="Nom"
                     autoFocus
                   />
                 </Grid> */}
 
                 {/* <Grid item xs={12} sm={6}>
-                  Téléphone:
-                  <TextField
-                    // {...AddChild("phone", {
-                    //   required: true,
-                    //   maxLength: {
-                    //     value: 20,
-                    //     message: "Nom: Longueur maximale de 20 caractères",
-                    //   },
-                    // })}
-                    value={phone}
-                    onChange={(event) => {
-                      setPhone(event.target.value);
-                    }}
-                    required
-                    fullWidth
-                    id="phone"
-                    autoFocus
-                  />
-                </Grid> */}
-
-                {/* <Grid item xs={12}>
-                  Adresse:
-                  <TextField
-                    // {...AddChild("address", {
-                    //   required: true,
-                    //   maxLength: {
-                    //     value: 20,
-                    //     message: "Code postal: Longueur maximale de 5 caractères",
-                    //   },
-                    // })}
-                    value={address}
-                    onChange={(event) => {
-                      setAddress(event.target.value);
-                    }}
-                    required
-                    fullWidth
-                    id="address"
-                    autoFocus
-                  />
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                      inputFormat="MM/DD/YYYY"
+                      label="Date de naissance"
+                      id="birthDate"
+                      // value={firstnameChild}
+                      //       onChange={(event) => {
+                      //         setFirstnameChild(event.target.value);
+                      //       }}
+                      value={birthDate}
+                      onChange={(newValue) => {
+                        setValue(newValue);
+                      }}
+                      renderInput={(params) => <TextField {...params} />}
+                    />
+                  </LocalizationProvider>
                 </Grid> */}
 
                 {/* <Grid item xs={12} sm={6}>
-                  Code postal:
-                  <TextField
-                    {...AddChild("postalCode", {
-                      required: true,
-                      maxLength: {
-                        value: 20,
-                        message:
-                          "Code postal: Longueur maximale de 5 caractères",
-                      },
-                    })}
-                    value={postalCode}
-                    onChange={(event) => {
-                      setPostalCode(event.target.value);
-                    }}
-                    required
-                    fullWidth
-                    id="postalCode"
-                    autoFocus
-                  />
-                </Grid> */}
-
-                {/* <Grid item xs={12} sm={6}>
-                  Ville:
-                  <TextField
-                    {...AddChild("city", {
-                      required: true,
-                      maxLength: {
-                        value: 20,
-                        message: "Ville: Longueur maximale de 20 caractères",
-                      },
-                    })}
-                    value={city}
-                    onChange={(event) => {
-                      setCity(event.target.value);
-                    }}
-                    required
-                    fullWidth
-                    id="city"
-                    // autoFocus
-                  />
+                  <Stack direction="row" alignItems="center" spacing={2}>
+                    <IconButton
+                      color="primary"
+                      aria-label="upload picture"
+                      component="label"
+                    >
+                      <input
+                        hidden
+                        accept="image/*"
+                        type="file"
+                        onChange={changeHandler}
+                          // value={imageChild}
+                          //   onChange={(event) => {
+                          //     setImageChild(event.target.value);
+                          //   }}
+                        id="imageChild"
+                      />
+                      <PhotoCamera />
+                    </IconButton>
+                  </Stack>
                 </Grid> */}
               </Grid>
 
