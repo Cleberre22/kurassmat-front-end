8f6343e53f0ac66d66db3fd306766cbf73f0a2a7
Cleberre22 <c.leberre@hotmail.fr>
2022-11-02 11:18:34 +0100

    Index Child

--- src/pages/children/Children.jsx
+++ src/pages/children/Children.jsx
@@ -10,44 +10,18 @@ import Avatar from "@mui/material/Avatar";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import ModeEditIcon from "@mui/icons-material/ModeEdit";
 import DeleteIcon from "@mui/icons-material/Delete";
-import ButtonGroup from "@mui/material/ButtonGroup";
+import PropTypes from 'prop-types';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Fab from '@mui/material/Fab';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Fade from '@mui/material/Fade';
 
 import MenuHeader from "../../components/auth/MenuHeader";
 import Fox from "../../components//Fox";
 import Prince from "../../components/Prince";
 
+
 const Children = () => {
-  const buttons = [
-    <Button key="one">One</Button>,
-    <Button key="two">Two</Button>,
-    <Button key="three">Three</Button>,
-  ];
-
-  // Get the button:
-  let mybutton = document.getElementById("myBtn");
-
-  // When the user scrolls down 20px from the top of the document, show the button
-  window.onscroll = function () {
-    scrollFunction();
-  };
-
-  function scrollFunction() {
-    if (
-      document.body.scrollTop > 20 ||
-      document.documentElement.scrollTop > 20
-    ) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
-  }
-
-  // When the user clicks on the button, scroll to the top of the document
-  function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  }
-
   const [children, setChildren] = useState([]);
 
   useEffect(() => {
@@ -68,279 +42,139 @@ const Children = () => {
       .then(displayChildren);
   };
 
+
+  function ScrollTop(props) {
+    const { children, window } = props;
+    // Note that you normally won't need to set the window ref as useScrollTrigger
+    // will default to window.
+    // This is only being set here because the demo is in an iframe.
+    const trigger = useScrollTrigger({
+      target: window ? window() : undefined,
+      disableHysteresis: true,
+      threshold: 100,
+    });
+  
+    const handleClick = (event) => {
+      const anchor = (event.target.ownerDocument || document).querySelector(
+        '#back-to-top-anchor',
+      );
+  
+      if (anchor) {
+        anchor.scrollIntoView({
+          block: 'center',
+        });
+      }
+    };
+  
+    return (
+      <Fade in={trigger}>
+        <Box
+          onClick={handleClick}
+          role="presentation"
+          sx={{ position: 'fixed', bottom: 30, right: 40 }}
+        >
+          {children}
+        </Box>
+      </Fade>
+    );
+  }
+  
+  ScrollTop.propTypes = {
+    children: PropTypes.element.isRequired,
+    /**
+     * Injected by the documentation to work in an iframe.
+     * You won't need it on your project.
+     */
+    window: PropTypes.func,
+  };
+
+
   return (
     <div className="indexChild">
       <MenuHeader />
       <Box className="main">
         <CssBaseline />
-        <Container className="containerProfil">
-          <h1 className="titleProfil">Liste des enfants</h1>
+        <Container className="containerProfil" sx={{ mt: 25 }}>
+          <h1 id="back-to-top-anchor" className="titleProfil">Liste des enfants</h1>
 
           <Box className="boxProfil">
             {children.map((child) => (
               <Box className="userCard">
-                <Box className="boxActionIndexChild">
-                  {/* <a
-                    className="linkEditProfil"
-                    href="/editProfil/"
-                    id="style-2"
-                    data-replace="Modifier mon profil"
-                  >
-                    <span>Modifier mon profil </span>
-                  </a> */}
-                  <ButtonGroup
-                    orientation="vertical"
-                    aria-label="vertical contained button group"
-                    variant="text"
-                    sx={{ m: 2 }}
-                  >
-                    
+                <Box className="boxIndexChild">
+                  <Grid container spacing={2}>
+                    <Grid item xs={12} sm={10}>
+                      <Box className="cardIndexChild">
+                        <Box className="avatarIndexChild">
+                          <Avatar
+                            className="avatar"
+                            sx={{ width: 100, height: 100 }}
+                            src={`http://localhost:8000/storage/uploads/${child.imageChild}`}
+                          />
+                        </Box>
+                        <Box className="boxInfoIndexChild">
+                          <Box className="boxNameIndexChild">
+                            <p>Nom et prénom: {child.lastnameChild} </p>
+                            <p> {child.firstnameChild}</p>
+                          </Box>
 
-                    <Button href="/children/show/${id}" key="one"><VisibilityIcon /></Button>
+                          <p>Date de naissance: {child.birthDate}</p>
+                        </Box>
+                      </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={2}>
+                      <Box>
+                        {" "}
+                        <Box
+                          className="buttonGroupIndexChild"
+                          orientation="vertical"
+                          variant="text"
+                          sx={{ m: 2 }}
+                        >
+                          <Button
+                            className="actionButtonIndexChild"
+                            href="/children/show/${id}"
+                            key="one"
+                          >
+                            <VisibilityIcon />
+                          </Button>
 
-                    <Button key="two" href="/children/edit/${id}"><ModeEditIcon /></Button>
+                          <Button
+                            className="actionButtonIndexChild"
+                            key="two"
+                            href="/children/edit/${id}"
+                          >
+                            <ModeEditIcon />
+                          </Button>
 
-                    <Button
-                      key="three"
-                  href="#"
-                      onClick={() => {
-                        deleteChild(child.id);
-                      }}
-                    >
-                      <DeleteIcon />
-                    </Button>
-
-                  </ButtonGroup>
-
-
-
-
-
-
-
+                          <Button
+                            className="actionButtonIndexChild"
+                            key="three"
+                            href="#"
+                            onClick={() => {
+                              deleteChild(child.id);
+                            }}
+                          >
+                            <DeleteIcon />
+                          </Button>
+                        </Box>
+                      </Box>
+                    </Grid>
+                  </Grid>
                 </Box>
-
-                <div className="cardIndexChild">
-                  <Avatar
-                    sx={{ width: 70, height: 70 }}
-                    src={`http://localhost:8000/storage/uploads/${child.imageChild}`}
-                  />
-                  <p>{child.firstnameChild} </p>
-                  <p> {child.lastnameChild}</p>
-                  <p> {child.birthDate}</p>
-
-               
-
-               
-
-                  
-                </div>
               </Box>
             ))}
-
-            {/* <Box component="form" onSubmit={AddChild}>
-              <Box className="userCardTopEdit" sx={{ mb: 4 }}>
-              <Avatar
-                        sx={{ width: 100, height: 100 }}
-                        src="avatar.png"
-                      />
-                <button type="submit" className="button-87" role="button">
-                 Ajouter une photo
-                </button>
-              </Box> */}
-
-            {/* <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
-                  Prénom:
-                  <TextField
-                    // {...AddChild("firstnameChild", {
-                    //   required: true,
-                    //   maxLength: {
-                    //     value: 20,
-                    //     message: "Prénom: Longueur maximale de 20 caractères",
-                    //   },
-                    // })}
-                    value={firstnameChild}
-                    onChange={(event) => {
-                      setFirstnameChild(event.target.value);
-                    }}
-                    required
-                    fullWidth
-                    id="firstnameChild"
-                    autoFocus
-                  />
-                </Grid>
-
-                <Grid item xs={12} sm={6}>
-                  Nom:
-                  <TextField
-                    // {...AddChild("lastnameChild", {
-                    //   required: true,
-                    //   maxLength: {
-                    //     value: 20,
-                    //     message: "Nom: Longueur maximale de 20 caractères",
-                    //   },
-                    // })}
-                    value={lastnameChild}
-                    onChange={(event) => {
-                      setLastnameChild(event.target.value);
-                    }}
-                    required
-                    fullWidth
-                    id="lastnameChild"
-                    autoFocus
-                  />
-                </Grid> */}
-
-            {/* {errors.firstname ? (
-                  <Alert
-                    className="errorsMessage"
-                    sx={{ mt: 2, p: 0, pl: 2 }}
-                    severity="error"
-                  >
-                    {errors.firstname?.message}
-                  </Alert>
-                ) : (
-                  ""
-                )}
-                {errors.lastname ? (
-                  <Alert
-                    className="errorsMessage"
-                    sx={{ mt: 2, p: 0, pl: 2 }}
-                    severity="error"
-                  >
-                    {errors.lastname?.message}
-                  </Alert>
-                ) : (
-                  ""
-                )} */}
-
-            {/* <Grid item xs={12} sm={6}>
-                  Email:
-                  <TextField
-                    {...AddChild("email", {
-                      required: true,
-                      maxLength: {
-                        value: 20,
-                        message: "Prénom: Longueur maximale de 20 caractères",
-                      },
-                    })}
-                    value={email}
-                    onChange={(event) => {
-                      setEmail(event.target.value);
-                    }}
-                    required
-                    fullWidth
-                    id="email"
-                    autoFocus
-                  />
-                </Grid> */}
-
-            {/* <Grid item xs={12} sm={6}>
-                  Téléphone:
-                  <TextField
-                    // {...AddChild("phone", {
-                    //   required: true,
-                    //   maxLength: {
-                    //     value: 20,
-                    //     message: "Nom: Longueur maximale de 20 caractères",
-                    //   },
-                    // })}
-                    value={phone}
-                    onChange={(event) => {
-                      setPhone(event.target.value);
-                    }}
-                    required
-                    fullWidth
-                    id="phone"
-                    autoFocus
-                  />
-                </Grid> */}
-
-            {/* <Grid item xs={12}>
-                  Adresse:
-                  <TextField
-                    // {...AddChild("address", {
-                    //   required: true,
-                    //   maxLength: {
-                    //     value: 20,
-                    //     message: "Code postal: Longueur maximale de 5 caractères",
-                    //   },
-                    // })}
-                    value={address}
-                    onChange={(event) => {
-                      setAddress(event.target.value);
-                    }}
-                    required
-                    fullWidth
-                    id="address"
-                    autoFocus
-                  />
-                </Grid> */}
-
-            {/* <Grid item xs={12} sm={6}>
-                  Code postal:
-                  <TextField
-                    {...AddChild("postalCode", {
-                      required: true,
-                      maxLength: {
-                        value: 20,
-                        message:
-                          "Code postal: Longueur maximale de 5 caractères",
-                      },
-                    })}
-                    value={postalCode}
-                    onChange={(event) => {
-                      setPostalCode(event.target.value);
-                    }}
-                    required
-                    fullWidth
-                    id="postalCode"
-                    autoFocus
-                  />
-                </Grid> */}
-
-            {/* <Grid item xs={12} sm={6}>
-                  Ville:
-                  <TextField
-                    {...AddChild("city", {
-                      required: true,
-                      maxLength: {
-                        value: 20,
-                        message: "Ville: Longueur maximale de 20 caractères",
-                      },
-                    })}
-                    value={city}
-                    onChange={(event) => {
-                      setCity(event.target.value);
-                    }}
-                    required
-                    fullWidth
-                    id="city"
-                    // autoFocus
-                  />
-                </Grid> */}
-            {/* </Grid>
-
-              <Grid
-                container
-                justifyContent="center"
-                sx={{ mt: 4 }}
-                className="buttonAddChild"
-              >
-                <button type="submit" className="button-877" role="button">
-                  Ajouter un enfant
-                </button>
-              </Grid> */}
-            {/* </Box> */}
           </Box>
         </Container>
+
+        <ScrollTop className="BackToTop">
+        <Fab className="BackToTop" size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon className="iconBackToTop"/>
+        </Fab>
+      </ScrollTop>
+
       </Box>
       <Fox />
       <Prince />
-      <button onclick="topFunction()" id="myBtn" title="Go to top">
-        Top
-      </button>
     </div>
   );
 };
--- src/pages/profil/ShowProfil.jsx
+++ src/pages/profil/ShowProfil.jsx
@@ -6,15 +6,7 @@ import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Avatar from "@mui/material/Avatar";
-import { Link } from "react-router-dom";
 
-import EditIcon from "@mui/icons-material/Edit";
-import AddIcon from "@mui/icons-material/Add";
-
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import ListSubheader from "@mui/material/ListSubheader";
 
 import MenuHeader from "../../components/auth/MenuHeader";
 import Fox from "../../components/Fox";
@@ -147,31 +139,6 @@ const Profil = () => {
                     </Box>
                   </Box>
 
-                  {/* <List
-                    sx={{
-                      width: "100%",
-                      maxWidth: 360,
-                      bgcolor: "background.paper",
-                      position: "relative",
-                      overflow: "auto",
-                      maxHeight: 300,
-                      "& ul": { padding: 0 },
-                    }}
-                    subheader={<li />}
-                  >
-                    {[0, 1, 2, 3, 4].map((sectionId) => (
-                      <li key={`section-${sectionId}`}>
-                        <ul>
-                          <ListSubheader>{`I'm sticky ${sectionId}`}</ListSubheader>
-                          {[0, 1, 2].map((item) => (
-                            <ListItem key={`item-${sectionId}-${item}`}>
-                              <ListItemText primary={`Item ${item}`} />
-                            </ListItem>
-                          ))}
-                        </ul>
-                      </li>
-                    ))}
-                  </List> */}
                 </Grid>
               </Grid>
             </Box>
