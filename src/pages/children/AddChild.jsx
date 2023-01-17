3ea021b97c9870caa258b1dc3b9ae8ce3dcdadb8
myrdinaptaliesen <64499780+myrdinaptaliesen@users.noreply.github.com>
2023-01-17 16:25:47 +0100

    Update AddChild.jsx

--- src/pages/children/AddChild.jsx
+++ src/pages/children/AddChild.jsx
@@ -82,13 +82,13 @@ const AddChild = () => {
 
     await axios
        .post(`https://kurassmat.charleyleberre.fr/api/childs/`, formData, {
-        headers: {
-      //     'Content-Type': 'multipart/form-data',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE, OPTIONS',
-          'Access-Control-Allow-Headers': 'Content-Type',
+      //   headers: {
+      // //     'Content-Type': 'multipart/form-data',
+      //     'Access-Control-Allow-Origin': '*',
+      //     'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE, OPTIONS',
+      //     'Access-Control-Allow-Headers': 'Content-Type',
 
-         }
+      //    }
    })
       .then(navigate("/"))
       .catch(({ response }) => {
